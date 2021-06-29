94bf8359e4c1504062477a260182923784478782
UNHabitatAndinos <73270842+UNHabitatAndinos@users.noreply.github.com>
2021-06-29 17:02:33 -0500

    commitf

--- src/map.js
+++ src/map.js
@@ -40,20 +40,37 @@ info.update = function (props) {
     this._div.innerHTML = (props ?
         'Municipio ' + props.MUNI + '<br />' +  '<br />' + 
 
+        '<b>Vivienda </b>' + '<br />' +
+        'Vivienda adecuada (material de paredes): ' + props.MAT_PARED.toFixed(0) + ' %' + '<br />' +
+        'Hacinamiento extremo: ' + props.IND_10.toFixed(0) + ' %' + '<br />' +
+        'Agua mejorada: ' + props.IND_8.toFixed(0) + ' %' + '<br />' +
+        'Saneamiento: ' + props.IND_11.toFixed(0) + ' %' + '<br />' +
+        'Electricidad: ' + props.IND_13.toFixed(0) + ' %' + '<br />' +
+        'Internet: ' + props.IND_19.toFixed(0) + ' %' + '<br />' +  '<br />' +  
+
         '<b>Salud</b>' + '<br />' +
-        'Proximidad centros de salud: ' + props.DxP_SALUD.toFixed(0) + ' m' + '<br />' +
-        'Concentración de Pm10: ' + props.PM10.toFixed(0) + ' µg/m3' +  '<br />' + '<br />' +    
-        
+        'Proximidad equipamientos de salud: ' + props.DxP_SALUD.toFixed(0) + ' m' + '<br />' +
+        'Concentración de Pm10: ' + props.PM10.toFixed(2) + ' µg/m3' +  '<br />' +   
+        'Contaminación residuos sólidos: ' + props.CON_SOL.toFixed(0) + ' %' + '<br />' +  '<br />' +   
+
         '<b>Educación, cultura y diversidad </b>' + '<br />' +
-        'Proximidad equipamientos educativos: ' + props.DxP_EDUCA.toFixed(0) + ' m' + '<br />' +   '<br />' + 
-        
+        'Proximidad equipamientos educativos: ' + props.DxP_EDUCA.toFixed(0) + ' m' + '<br />' +  '<br />' +  
+
         '<b>Espacios públicos, seguridad y recreación </b>' + '<br />' +
-        'Proximidad espacio público: ' + props.DxP_EP.toFixed(0) + ' m'+ '<br />' +   '<br />' + 
+        'Proximidad espacio público: ' + props.DxP_EP.toFixed(0) + ' m' + '<br />' +
+        'M² per capita de espacio público: ' + props.M2_EP_CA.toFixed(2) + '<br />' +
+        'Densidad poblacional: ' + props.IND_14.toFixed(2) + '<br />' +
+        'Tasa de hurtos x 100mil habitantes: ' + props.IND_26.toFixed(1) + '<br />' +
+        'Tasa de homicidios x 100mil habitantes: ' + props.IND_25.toFixed(1) + '<br />' +
+        'Diversidad usos del suelo: ' + props.IND_30.toFixed(3) + '/1.61' +'<br />' + '<br />' +
 
         '<b>Oportunidades económicas </b>' + '<br />' +
-        'Proximidad unidades de servicios y comerciales: ' + props.DxP_CENTR.toFixed(0) + ' m'  : 'Seleccione una manzana');
+        'Proximidad unidades servicios y comerciales: ' + props.DxP_CENTR.toFixed(0) + ' m' + '<br />' +
+        'Tasa de desempleo: ' + props.IND_2.toFixed(2) + ' %' + '<br />' +
+        'Empleo informal estricto: ' + props.IND_3.toFixed(2) + ' %' : 'Seleccione una manzana');
 };
 info.addTo(map);
+
 
 function stylec(feature) {
     return {
@@ -140,7 +157,7 @@ function changeLegend(props) {
 var legends = {
     DxP_SALUD: {
         title: "Proximidad equipamientos de salud",
-        subtitle: "Distancia en m x Factor inclinación del terreno", 
+        subtitle: "Distancia en metros con factor de inclinación del terreno", 
         elem1: '<div><span  style= "color:#1a9641">▉</span>0 - 300</div>',
         elem2: '<div><span  style= "color:#a6d96a">▉</span>301 - 500</div>', 
         elem3: '<div><span  style= "color:#f4f466">▉</span>501 - 1000</div>',
@@ -152,7 +169,7 @@ var legends = {
     },
     DxP_EDUCA: {
         title: "Proximidad equipamientos de educación",
-        subtitle: "Distancia en m x Factor inclinación del terreno", 
+        subtitle: "Distancia en metros con factor de inclinación del terreno", 
         elem1: '<div><span  style= "color:#1a9641">▉</span>0 - 300</div>',
         elem2: '<div><span  style= "color:#a6d96a">▉</span>301 - 500</div>', 
         elem3: '<div><span  style= "color:#f4f466">▉</span>501 - 1000</div>',
@@ -164,7 +181,7 @@ var legends = {
     },
     DxP_EP: {
         title: "Proximidad espacio público",
-        subtitle: "Distancia en m x Factor inclinación del terreno", 
+        subtitle: "Distancia en metros con factor de inclinación del terreno", 
         elem1: '<div><span  style= "color:#1a9641">▉</span>0 - 500</div>',
         elem2: '<div><span  style= "color:#a6d96a">▉</span>501 - 1000</div>', 
         elem3: '<div><span  style= "color:#f4f466">▉</span>1001 - 5000</div>',
@@ -187,8 +204,8 @@ var legends = {
         elem8: "Ministerio de Ambiente y Recursos Naturales",
     },
     DxP_CENTR: {
-        title: "Proximidad unidades de servicio y comerciales",
-        subtitle: "Distancia en m x Factor inclinación del terreno", 
+        title: "Proximidad zonas de interés económico (servicios y comercio)",
+        subtitle: "Distancia en metros con factor de inclinación del terreno", 
         elem1: '<div><span  style= "color:#1a9641">▉</span>0 - 500</div>',
         elem2: '<div><span  style= "color:#a6d96a">▉</span>501 - 1500</div>', 
         elem3: '<div><span  style= "color:#f4f466">▉</span>1501 - 3000</div>',
@@ -198,13 +215,181 @@ var legends = {
         elem7: '',
         elem8: "Google Maps",
     },
+    MAT_PARED: {
+        title: "Vivienda con material de paredes adecuado",
+        subtitle: "% de Viviendas", 
+        elem1: '<div><span  style= "color:#1a9641">▉</span>98.7 - 99.9</div>',
+        elem2: '<div><span  style= "color:#a6d96a">▉</span>97.3 - 98.6</div>', 
+        elem3: '<div><span  style= "color:#f4f466">▉</span>97.1 - 97.2</div>',
+        elem4: '<div><span  style= "color:#fdae61">▉</span>93.1 - 97.0</div>',
+        elem5: '<div><span  style= "color:#d7191c">▉</span>92.8 - 93.0</div>',
+        elem6: '',
+        elem7: '',
+        elem8: "ONE - Censo Nacional Población y Vivienda 2010",
+    },
+    IND_13: {
+        title: "Acceso a electricidad",
+        subtitle: "% Hogares", 
+        elem1: '<div><span  style= "color:#1a9641">▉</span>99.38 - 99.76</div>',
+        elem2: '<div><span  style= "color:#a6d96a">▉</span>98.59 - 99.37</div>', 
+        elem3: '<div><span  style= "color:#f4f466">▉</span>98.10 - 95.58</div>',
+        elem4: '<div><span  style= "color:#fdae61">▉</span>97.96 - 98.09</div>',
+        elem5: '<div><span  style= "color:#d7191c">▉</span>96.40 - 97.95</div>',
+        elem6: '',
+        elem7: '',
+        elem8: "ONE - Censo Nacional Población y Vivienda 2010",
+    },
+    IND_14: {
+        title: "Densidad residencial",
+        subtitle: "Población x km2", 
+        elem1: '<div><span  style= "color:#1a9641">▉</span>189 - 664</div>',
+        elem2: '<div><span  style= "color:#a6d96a">▉</span>665 - 1670</div>', 
+        elem3: '<div><span  style= "color:#f4f466">▉</span>1671 - 3473</div>',
+        elem4: '<div><span  style= "color:#fdae61">▉</span>3474 - 8235</div>',
+        elem5: '<div><span  style= "color:#d7191c">▉</span>8236 - 11339</div>',
+        elem6: '',
+        elem7: '',
+        elem8: "ONE - Censo Nacional Población y Vivienda 2010",
+    },
+    IND_8: {
+        title: "Acceso a agua mejorada",
+        subtitle: "% de Hogares", 
+        elem1: '<div><span  style= "color:#1a9641">▉</span>82.11 - 94.13</div>',
+        elem2: '<div><span  style= "color:#a6d96a">▉</span>79.87 - 82.10</div>', 
+        elem3: '<div><span  style= "color:#f4f466">▉</span>73.39 - 79.86</div>',
+        elem4: '<div><span  style= "color:#fdae61">▉</span>59.75 - 73.38</div>',
+        elem5: '<div><span  style= "color:#d7191c">▉</span>57.98 - 59.74</div>',
+        elem6: '',
+        elem7: '',
+        elem8: "ONE - Censo Nacional Población y Vivienda 2010",
+    },
+    IND_10: {
+        title: "Hacinamiento extremo",
+        subtitle: "% de Hogares", 
+        elem1: '<div><span  style= "color:#1a9641">▉</span>2.86 - 3.16</div>',
+        elem2: '<div><span  style= "color:#a6d96a">▉</span>3.17 - 3.50</div>', 
+        elem3: '<div><span  style= "color:#f4f466">▉</span>3.51 - 3.80</div>',
+        elem4: '<div><span  style= "color:#fdae61">▉</span>3.81 - 4.28</div>',
+        elem5: '<div><span  style= "color:#d7191c">▉</span>4.29 - 4.71</div>',
+        elem6: '',
+        elem7: '',
+        elem8: "ONE - Censo Nacional Población y Vivienda 2010",
+    },
+    IND_11: {
+        title: "Alto grado de saneamiento",
+        subtitle: "% de Hogares", 
+        elem1: '<div><span  style= "color:#1a9641">▉</span>56.01 - 62.55</div>',
+        elem2: '<div><span  style= "color:#a6d96a">▉</span>42.19 - 56.00</div>', 
+        elem3: '<div><span  style= "color:#f4f466">▉</span>38.98 - 42.18</div>',
+        elem4: '<div><span  style= "color:#fdae61">▉</span>24.21 - 38.97</div>',
+        elem5: '<div><span  style= "color:#d7191c">▉</span>15.46 - 24.20</div>',
+        elem6: '',
+        elem7: '',
+        elem8: "ONE - Censo Nacional Población y Vivienda 2010",
+    },
+    IND_19: {
+        title: "Acceso a internet",
+        subtitle: "% de Hogares", 
+        elem1: '<div><span  style= "color:#1a9641">▉</span>23.01 - 33.29</div>',
+        elem2: '<div><span  style= "color:#a6d96a">▉</span>13.87 - 23.00</div>', 
+        elem3: '<div><span  style= "color:#f4f466">▉</span>8.23 - 13.86</div>',
+        elem4: '<div><span  style= "color:#fdae61">▉</span>4.05 - 8.22</div>',
+        elem5: '<div><span  style= "color:#d7191c">▉</span>3.72 - 4.04</div>',
+        elem6: '',
+        elem7: '',
+        elem8: "ONE - Censo Nacional Población y Vivienda 2010",
+    },
+    IND_25: {
+        title: "Tasa de homicidios",
+        subtitle: "Homicidios x 100mil habitantes",
+        elem1: '<div><span  style= "color:#1a9641">▉</span>2.90 - 8.10</div>',
+        elem2: '<div><span  style= "color:#a6d96a">▉</span>8.11 - 9.80</div>', 
+        elem3: '<div><span  style= "color:#f4f466">▉</span>9.81 - 11.90</div>',
+        elem4: '<div><span  style= "color:#fdae61">▉</span>11.91 - 17.30</div>',
+        elem5: '<div><span  style= "color:#d7191c">▉</span>17.31 - 22.50</div>',
+        elem6: '',
+        elem7: '',
+        elem8: "Encuesta Nacional Continua de Fuerza de Trabajo 2019",
+    },
+    IND_26: {
+        title: "Tasa de hurtos",
+        subtitle: "Hurtos x 100mil habitantes",
+        elem1: '<div><span  style= "color:#1a9641">▉</span>5.97 - 36.46</div>',
+        elem2: '<div><span  style= "color:#a6d96a">▉</span>5.98 - 66.28</div>', 
+        elem3: '<div><span  style= "color:#f4f466">▉</span>66.29 - 117.00</div>',
+        elem4: '<div><span  style= "color:#fdae61">▉</span>117.01 - 122.36</div>',
+        elem5: '<div><span  style= "color:#d7191c">▉</span>122.37 - 187.41</div>',
+        elem6: '',
+        elem7: '',
+        elem8: "Encuesta Nacional Continua de Fuerza de Trabajo 2019",
+    },
+    IND_2: {
+        title: "Tasa de desempleo",
+        subtitle: "% de Personas", 
+        elem1: '<div><span  style= "color:#1a9641">▉</span>5.81 - 6.60</div>',
+        elem2: '<div><span  style= "color:#a6d96a">▉</span>6.61 - 7.00</div>', 
+        elem3: '<div><span  style= "color:#f4f466">▉</span>7.01 - 7.73</div>',
+        elem4: '<div><span  style= "color:#fdae61">▉</span>7.74 - 8.10</div>',
+        elem5: '<div><span  style= "color:#d7191c">▉</span>8.11 - 8.60</div>',
+        elem6: '',
+        elem7: '',
+        elem8: "ONE - Censo Nacional Población y Vivienda 2010",
+    },
+    IND_3: {
+        title: "Tasa de empleo informal",
+        subtitle: "% de Personas", 
+        elem1: '<div><span  style= "color:#1a9641">▉</span>29.76 - 30.41</div>',
+        elem2: '<div><span  style= "color:#a6d96a">▉</span>30.42 - 30.91</div>', 
+        elem3: '<div><span  style= "color:#f4f466">▉</span>30.92 - 33.04</div>',
+        elem4: '<div><span  style= "color:#fdae61">▉</span>33.05 - 33.65</div>',
+        elem5: '<div><span  style= "color:#d7191c">▉</span>33.66 - 36.68</div>',
+        elem6: '',
+        elem7: '',
+        elem8: "ONE - Censo Nacional Población y Vivienda 2010",
+    },
+    M2_EP_CA: {
+        title: "M² per capita de espacio público",
+        subtitle: "m²/habitante",
+        elem1: '<div><span  style= "color:#1a9641">▉</span>2.84 - 6.31</div>',
+        elem2: '<div><span  style= "color:#a6d96a">▉</span>1.89 - 2.83</div>', 
+        elem3: '<div><span  style= "color:#f4f466">▉</span>0.72 - 1.88</div>',
+        elem4: '<div><span  style= "color:#fdae61">▉</span>0.29 - 0.71</div>',
+        elem5: '<div><span  style= "color:#d7191c">▉</span>0.09 - 0.28</div>',
+        elem6: '',
+        elem7: '',
+        elem8: "Google Earth - Imagen Satelital Landsat",
+    },
+    CON_SOL: {
+        title: "Contaminación residuos sólidos",
+        subtitle: "% de Hogares",
+        elem1: '<div><span  style= "color:#1a9641">▉</span>5 - 6</div>',
+        elem2: '<div><span  style= "color:#a6d96a">▉</span>7 - 8</div>', 
+        elem3: '<div><span  style= "color:#f4f466">▉</span>9 - 14</div>',
+        elem4: '<div><span  style= "color:#fdae61">▉</span>15 - 16</div>',
+        elem5: '<div><span  style= "color:#d7191c">▉</span>17 - 31</div>',
+        elem6: '',
+        elem7: '',
+        elem8: "ONE - Censo Nacional Población y Vivienda 2010",
+    },
+    IND_30: {
+        title: "Diversidad usos del suelo",
+        subtitle: "Índice de Shanon -  Nivel de diversidad por manzana", 
+        elem1: '<div><span  style= "color:#1a9641">▉</span>0.141 - 0.280</div>',
+        elem2: '<div><span  style= "color:#a6d96a">▉</span>0.131 - 0.140</div>',
+        elem3: '<div><span  style= "color:#f4f466">▉</span>0.041 - 0.130</div>',
+        elem4: '<div><span  style= "color:#fdae61">▉</span>0.036 - 0.040</div>',
+        elem5: '<div><span  style= "color:#d7191c">▉</span>0.030 - 0.035</div>',
+        elem6: '',
+        elem7: '',
+        elem8: "Gobierno local",
+    },
 }
 
 var indi = L.geoJson(Manzana, {
-    style: legends.DxP_SALUD,
+    style: legends.IND_14,
 }).addTo(map);
 
-var currentStyle = 'DxP_SALUD';
+var currentStyle = 'IND_14';
 
 manzanas = L.geoJson(Manzana, {
     style: style,
@@ -238,6 +423,104 @@ function setProColor(d) {
             d > 3000 ? '#fdae61' :
                 d > 1500 ? '#f4f466' :
                     d > 500 ? '#a6d96a' :
+                    '#1a9641';
+    }
+    else if (currentStyle === 'MAT_PARED') {
+        return d > 98.6 ? '#1a9641' :
+            d > 97.2 ? '#a6d96a' :
+                d > 97 ? '#f4f466' :
+                    d > 93 ? '#fdae61' :
+                    '#d7191c';
+    }
+    else if (currentStyle === 'IND_13') {
+        return d > 99.37 ? '#1a9641' :
+            d > 98.58? '#a6d96a' :
+                d > 98.09 ? '#f4f466' :
+                    d > 97.95 ? '#fdae61' :
+                    '#d7191c';
+    }
+    else if (currentStyle === 'IND_14') {
+        return d > 8235 ? '#d7191c' :
+            d > 3473 ? '#fdae61' :
+                d > 1670 ? '#f4f466' :
+                    d > 664 ? '#a6d96a' :
+                    '#1a9641';
+    }
+    else if (currentStyle === 'IND_8') {
+        return d > 82.10 ? '#1a9641' :
+            d > 79.86 ? '#a6d96a' :
+                d > 73.38 ? '#f4f466' :
+                    d > 59.74 ? '#fdae61' :
+                    '#d7191c';
+    }
+    else if (currentStyle === 'IND_10') {
+        return d > 4.28 ? '#d7191c' :
+            d > 3.80 ? '#fdae61' :
+                d > 3.50 ? '#f4f466' :
+                    d > 3.16 ? '#a6d96a' :
+                    '#1a9641';
+    }
+    else if (currentStyle === 'IND_11') {
+        return d > 56 ? '#d7191c' :
+            d > 42.18 ? '#fdae61' :
+                d > 38.97 ? '#f4f466' :
+                    d > 24.20 ? '#a6d96a' :
+                    '#1a9641';
+    }
+    else if (currentStyle === 'IND_19') {
+        return d > 23 ? '#1a9641' :
+            d > 13.86 ? '#a6d96a' :
+                d > 8.22 ? '#f4f466' :
+                    d > 4.04 ? '#fdae61' :
+                    '#d7191c';
+    }
+    else if (currentStyle === 'IND_25') {
+        return d > 17.3 ? '#d7191c' :
+            d > 11.9 ? '#fdae61' :
+                d > 9.8 ? '#f4f466' :
+                    d > 8.1 ? '#a6d96a' :
+                    '#1a9641';
+    }
+    else if (currentStyle === 'IND_26') {
+        return d > 122.36 ? '#d7191c' :
+            d > 117 ? '#fdae61' :
+                d > 66.28 ? '#f4f466' :
+                    d > 36.46 ? '#a6d96a' :
+                    '#1a9641';
+    }
+    else if (currentStyle === 'IND_2') {
+        return d > 8.1 ? '#d7191c' :
+            d > 7.73 ? '#fdae61' :
+                d > 7 ? '#f4f466' :
+                    d > 6.6 ? '#a6d96a' :
+                    '#1a9641';
+    }
+    else if (currentStyle === 'IND_3') {
+        return d > 33.65 ? '#d7191c' :
+            d > 33.04 ? '#fdae61' :
+                d > 30.91 ? '#f4f466' :
+                    d > 30.41 ? '#a6d96a' :
+                    '#1a9641';
+    }
+    else if (currentStyle === 'M2_EP_CA') {
+        return d > 2.83 ? '#1a9641' :
+            d > 1.88 ? '#a6d96a' :
+                d > 0.71 ? '#f4f466' :
+                    d > 0.28 ? '#fdae61' :
+                    '#d7191c';
+    }
+    else if (currentStyle === 'IND_30') {
+        return d > 0.140 ? '#1a9641' :
+            d > 0.130 ? '#a6d96a' :
+                d > 0.040 ? '#f4f466' :
+                    d > 0.035 ? '#fdae61' :
+                    '#d7191c';
+    }
+    else if (currentStyle === 'CON_SOL') {
+        return d > 16 ? '#d7191c' :
+            d > 14 ? '#fdae61' :
+                d > 8 ? '#f4f466' :
+                    d > 6 ? '#a6d96a' :
                     '#1a9641';
     }
     else {
@@ -287,7 +570,7 @@ var layersControl = new L.Control.Layers(baseMaps, overlayMaps, {
     collapsed: true,
 });
 map.addControl(layersControl);
-changeIndi({value: 'DxP_SALUD'});
+changeIndi({value: 'IND_14'});
 
 function popupText(feature, layer) {
     layer.bindPopup('Municipio ' + feature.properties.NAME + '<br />')
